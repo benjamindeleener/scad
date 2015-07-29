9cac8db865ffcaa3a26209a4935ccc0c6dbedc0e
Tanguy Duval <duvaltanguy@gmail.com>
2015-07-29 16:56:54 -0400

    Cosmetic

--- scripts/SpinalCordDetection/scad_minimalpath_homo.m
+++ scripts/SpinalCordDetection/scad_minimalpath_homo.m
@@ -2,4 +2,5 @@ function scad_minimalpath_homo(fname)
 % scad_minimalpath(fname)
 nii=load_nii(fname); 
 [~,~,S]=minimalPath3d(double(nii.img),1,1,0); S(S>prctile(S(:),50))=prctile(S(:),50); S=(S-min(min(min(S))))/max(max(max(S)));
-save_nii_v2(1-S,[sct_tool_remove_extension(fname,1) '_minimalpath.nii'],fname,64)
+save_nii_v2(1-S,[sct_tool_remove_extension(fname,1) '_minimalpath.nii.gz'],fname,64)
+disp(['unix(''fslview ' fname ' ' sct_tool_remove_extension(fname,1) '_minimalpath -l "render3" -t 0.7 -b 0.7,1'')'])
--- scripts/SpinalCordDetection/scad_symetry.m
+++ scripts/SpinalCordDetection/scad_symetry.m
@@ -5,13 +5,13 @@ u=load_nii(fname);
 clear out
 p=squeeze(sum(u.img,2));
 for i=1:size(p,2)
-correlation=xcorr(p(:,i),p(end:-1:1,i));
-out(:,i)=msnorm(transpose(1:length(correlation)),correlation(:),'Max',1);
+    out(:,i)=xcorr(p(:,i),p(end:-1:1,i));
+    out(:,i)=out(:,i)./max(out(:,i));
 end
 % halfing
 index1=round(linspace(1,size(out,1)-1,u.dims(1)));
 index2=round(linspace(2,size(out,1),u.dims(1)));
 out=1/2*(out(index1,:)+out(index2,:));
 
-save_nii_v2(repmat(permute(out,[1 3 2]),[1 u.dims(2) 1]),[sct_tool_remove_extension(fname,1),'_symetry'],fname,64)
-disp(['unix(''fslview ' fname ' ' sct_tool_remove_extension(fname,1) '_symetry -l "Red" -t 0.5'')'])
+save_nii_v2(repmat(permute(out,[1 3 2]),[1 u.dims(2) 1]),[sct_tool_remove_extension(fname,1),'_symetry.nii.gz'],fname,64)
+disp(['unix(''fslview ' fname ' ' sct_tool_remove_extension(fname,1) '_symetry -l "Red" -t 0.5 -b 0.8,1'')'])
