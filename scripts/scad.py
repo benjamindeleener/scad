00ef81ab2b2c1fe406fc85b8778255182d72c694
Olivier Comtois <olcoma@jarrett.neuro.polymtl.ca>
2015-09-04 08:35:23 -0400

    Improved scad implementation. Not final version

--- scripts/scad.py
+++ scripts/scad.py
@@ -36,6 +36,37 @@ def treshold_min_path(data, threshold):
 def watershed_segmentation(img_data):
     pass
 
+def get_line_cross_corr():
+    pass
+
+
+def block_symmetry(img, section_nb):
+    """
+    This function splits an image in section_nb number of blocks along the A-P axis and calculates the l-r symmetry for each of them. Then, concatenantes them to provide the symmetry of the image
+    :param img: the image
+    :param section_nb: number of sections that the img will be divided into
+    :return: Concatenated sections, each having its own symmetry
+    """
+    raw_orientation = img.change_orientation()
+    dim = img.data.shape
+
+    section_length = dim[1]/section_nb
+
+    result = np.zeros(dim)
+
+    for i in range(0, section_nb):
+        if (i+1)*section_length > dim[1]:
+            y_length = (i+1)*section_length - ((i+1)*section_length - dim[1])
+            result[:, i*section_length:i*section_length + y_length, :] = symmetry_detector_right_left(img.data[:, i*section_length:i*section_length + y_length, :], (img.data[:, i*section_length:i*section_length + y_length, :]).shape, cropped_xy=1)
+        sym = symmetry_detector_right_left(img.data[:, i*section_length:(i+1)*section_length, :], (img.data[:, i*section_length:(i+1)*section_length, :]).shape, cropped_xy=1)
+        result[:, i*section_length:(i+1)*section_length, :] = sym
+
+    return result
+
+
+def mixed_symmetry():
+    pass
+
 
 def equalize_array_histogram(array):
     array_min = np.amin(array)
@@ -54,7 +85,7 @@ def get_minimum_path(data, smooth_factor=np.sqrt(2), invert=0, verbose=1):
     J1 = np.ones([m, n, p])*np.inf
     J2 = J1
     J1[:, :, 0] = 0
-    for row in range(1, p-1):
+    for row in range(0, p-1):
         pJ = J1[:, :, row-1]
         cP = np.squeeze(data[1:-2, 1:-2, row])
         # cP = np.squeeze(data[:, :, row])
@@ -85,8 +116,9 @@ def get_minimum_path(data, smooth_factor=np.sqrt(2), invert=0, verbose=1):
         result = np.divide(np.subtract(result, result_min), result_max)
         result_max = np.amax(result)
 
-    for i in range(1,p-1):
-        result[:,:,i] = 1 - result[:,:,i]
+
+    for i in range(1, p-2):
+        result[:, :, i] = 1 - result[:,:,i]
 
     return result
 
@@ -142,7 +174,7 @@ def remove_outliers(img_centerline_unsmoothed, std=None,mu=None ):
     if std is None and mu is None:
         mu, std = norm.fit(distance)
 
-    removed_outlier = False
+    # removed_outlier = False
 
     for i in range(z_max/2, z_max):
         if distance_phys[i] > 10:
@@ -150,10 +182,10 @@ def remove_outliers(img_centerline_unsmoothed, std=None,mu=None ):
             X1,Y1 = ind2sub(centerline_unsmoothed[:, :, i].shape,ind1)
             ind2 = np.argmax(centerline_unsmoothed[:, :, i+1])
             X2,Y2 = ind2sub(centerline_unsmoothed[:, :, i+1].shape,ind2)
-            centerline_unsmoothed[X2,Y2,i+1] = 0
-            if np.amax(centerline_unsmoothed[:, :, i+1]) == 0:
-                centerline_unsmoothed[X1,Y1,i+1] = 1
-            removed_outlier = True
+            centerline_unsmoothed[:,:,i+1] = 0
+            # if np.amax(centerline_unsmoothed[:, :, i+1]) == 0:
+            #     centerline_unsmoothed[X1,Y1,i+1] = 1
+            # removed_outlier = True
 
     for i in range((z_max/2)-1, 0, -1):
         if distance_phys[i] > 10:
@@ -161,14 +193,14 @@ def remove_outliers(img_centerline_unsmoothed, std=None,mu=None ):
             X1,Y1 = ind2sub(centerline_unsmoothed[:, :, i].shape,ind1)
             ind2 = np.argmax(centerline_unsmoothed[:, :, i-1])
             X2,Y2 = ind2sub(centerline_unsmoothed[:, :, i-1].shape,ind2)
-            centerline_unsmoothed[X2, Y2, i-1] = 0
-            if np.amax(centerline_unsmoothed[:, :, i-1]) == 0:
-                centerline_unsmoothed[X1, Y1, i-1] = 1
-            removed_outlier = True
+            centerline_unsmoothed[:, :, i-1] = 0
+            # if np.amax(centerline_unsmoothed[:, :, i-1]) == 0:
+            #     centerline_unsmoothed[X1, Y1, i-1] = 1
+            # removed_outlier = True
 
-    if removed_outlier:
-        img_centerline_unsmoothed.data = centerline_unsmoothed
-        centerline_unsmoothed = remove_outliers(img_centerline_unsmoothed, std, mu)
+    # if removed_outlier:
+    #     img_centerline_unsmoothed.data = centerline_unsmoothed
+    #     centerline_unsmoothed = remove_outliers(img_centerline_unsmoothed, std, mu)
 
     return centerline_unsmoothed
 
@@ -185,10 +217,30 @@ def remove_outliers(img_centerline_unsmoothed, std=None,mu=None ):
     # return norms
 
 
-def symmetry_detector_right_left(data, dim):
-    slice_p = np.squeeze(np.sum(data, 1))
+def symmetry_detector_right_left(data, dim, y_crop_elevation=0, cropped_xy=0):
+    """
+    This function computes the cross correlation for L-R body symmetry for an RPI image
+    This function expects an RPI image
+    :param data: input image data
+    :param dim: dimension of image
+    :param y_crop_elevation: if the SC is not included when cropping on A-P, then set this parameter to include it. This parameter value is in pixel # TODO:Give a better explanation
+    :return: L-R body symmetry (normalised between 0 and 1)
+    """
+    from scipy.ndimage.filters import gaussian_filter
+    img_data = gaussian_filter(data, [0, 5, 5])
+
+    if cropped_xy:
+        x_mid = np.round(dim[0]/2)
+        x_crop_min = int(x_mid - (0.35/2)*dim[0])
+        x_crop_max = int(x_mid + (0.35/2)*dim[0])
+
+        img_data[0:x_crop_min,:,:] = 0
+        img_data[x_crop_max:-1,:,:] = 0
+
+    slice_p = np.squeeze(np.sum(img_data, 1))
+    # slice_p = np.squeeze(np.sum(img_data[x_crop_min:x_crop_max,:,:], 1)) # test
     slice_p_reversed = np.flipud(slice_p)
-    m, n= slice_p.shape
+    m, n = slice_p.shape
     cross_corr = ((2*m)-1, n)
     cross_corr = np.zeros(cross_corr)
     for iz in range(0, np.size(slice_p[1])):
@@ -196,8 +248,11 @@ def symmetry_detector_right_left(data, dim):
         corr2 = slice_p_reversed[:, iz]
         cross_corr[:, iz] = np.double(np.correlate(corr1, corr2, "full"))
         max_value = np.max(cross_corr[:, iz])
-        cross_corr[:, iz] = cross_corr[:, iz]/max_value
-    data_out = np.zeros((m, n))
+        if max_value == 0:
+            cross_corr[:, iz] = 0
+        else:
+            cross_corr[:, iz] = cross_corr[:, iz]/max_value
+    data_out = np.zeros((dim[0], dim[2]))
     index1 = np.round(np.linspace(0,2*m-3, m))
     index2 = np.round(np.linspace(1,2*m-2, m))
     for i in range(0,m):
@@ -207,6 +262,7 @@ def symmetry_detector_right_left(data, dim):
         out2 = cross_corr[indx2, :]
         data_out[i, :] = 0.5*(out1 + out2)
     result = np.hstack([data_out[:, np.newaxis, :] for i in range(0, dim[1])])
+
     return result
 
 
@@ -298,7 +354,7 @@ class Script(BaseScript):
 
 
 class SCAD(Algorithm):
-    def __init__(self, input_image, contrast=None, verbose=0):
+    def __init__(self, input_image, contrast=None, verbose=1):
         super(SCAD, self).__init__(input_image)
         self._contrast = contrast
         self._verbose = verbose
@@ -333,16 +389,20 @@ class SCAD(Algorithm):
         sct.run('cp imageVesselNessFilter.nii.gz '+path_tmp+'imageVesselNessFilter.nii.gz')
         os.chdir(path_tmp)
 
-        # TODO: SymmetryDetector
-        # sct.run(matlab_run + ' "scad_symetry(\'raw.nii\'); exit"')
         img = Image("raw.nii")
         raw_orientation = img.change_orientation()
-        ap_sym = symmetry_detector_anterior_posterior(img.data, img.dim)
+        block_sym = block_symmetry(img, img.dim[1]/3)
         img.data = symmetry_detector_right_left(img.data, img.dim)
         img.change_orientation(raw_orientation)
         img.file_name += "_symetry"
         img.save()
         raw_symetry_data = img.data
+        img.data = block_sym
+        img.change_orientation(raw_orientation)
+        img.file_name = "block_symmetry"
+        img.save()
+
+
         # img.data = ap_sym
         # img.change_orientation(raw_orientation)
         # img.file_name += "_ap_symetry"
@@ -355,15 +415,14 @@ class SCAD(Algorithm):
         img.change_orientation()
         img.file_name = "min_path"
         img.save()
-        treshold_minimum_path = treshold_min_path(img.data, 0.95)
-        normalised_symmetry = equalize_array_histogram(raw_symetry_data)
-        enhanced_symmetry = np.power(normalised_symmetry, 2)
-        spine_detect_data = np.multiply(minimum_path_data, enhanced_symmetry)
+        # treshold_minimum_path = treshold_min_path(img.data, 0.95)
+        normalised_symmetry = equalize_array_histogram(block_sym)
+        # enhanced_symmetry = np.power(normalised_symmetry, 2)
+        spine_detect_data = np.multiply(minimum_path_data, normalised_symmetry)
         img.data = spine_detect_data
         img.change_orientation('RPI')
         img.file_name = "symetry_weighted_min_path"
         img.save()
-
 
         centerline_unsmoothed = get_centerline(spine_detect_data, spine_detect_data.shape)
         img.data = centerline_unsmoothed
@@ -376,8 +435,8 @@ class SCAD(Algorithm):
         img.file_name = "centerline_unsmoothed"
         img.save()
 
-
-        x, y, z, dx, dy, dz = smooth_centerline("centerline_unsmoothed.nii.gz")
+        x, y, z, dx, dy, dz = smooth_centerline("centerline_unsmoothed_outliers.nii.gz")
+        # x, y, z, dx, dy, dz = smooth_centerline("centerline_unsmoothed.nii.gz")
 
         centerline_dim = img.dim
 
@@ -437,33 +496,55 @@ class SCAD(Algorithm):
         if self.verbose:
             img.data = minimum_path_data
             img.change_orientation(raw_orientation)
-            img.file_name += "_min_path"
+            img.file_name = "min_path"
             img.save()
 
         # multiply normalised symmetry data with the minimum path result
-        spine_detect_data = np.multiply(minimum_path_data, np.power(equalize_array_histogram(raw_symetry_data),2))
+        spine_detect_data = np.multiply(minimum_path_data, equalize_array_histogram(raw_symetry_data))
         if self.verbose:
             img.data = spine_detect_data
             img.change_orientation('RPI')
             img.file_name = "symetry_weighted_min_path"
             img.save()
 
+        # symmetry_iter = symmetry_detector_right_left(spine_detect_data, spine_detect_data.shape) # test
+        # if self.verbose:
+        #     img.data = symmetry_iter
+        #     img.change_orientation()
+        #     img.file_name = "symmetry_iter"
+        #     img.save()
         # get the centerline from the minimal path
         img.data = get_centerline(spine_detect_data, spine_detect_data.shape)
+        img.file_name = "centerline_unsmoothed"
+        img.save()
+
+        # spine_detect_data = np.multiply(minimum_path_data, equalize_array_histogram(symmetry_iter))
+        # img.data = get_centerline(spine_detect_data, spine_detect_data.shape)
+        # img.file_name = "centerline_symmetry_iter"
+        # img.save()
+
+        x, y, z, dx, dy, dz = smooth_centerline("centerline_unsmoothed.nii.gz")
+
+        centerline_dim = img.dim
+        img.data = np.zeros(centerline_dim)
+        for i in range(0, np.size(x)-1):
+            img.data[int(x[i]), int(y[i]), int(z[i])] = 1
+
         img.change_orientation(raw_orientation)
         img.file_name = "centerline"
         img.save()
 
         # copy back centerline
         os.chdir('../')
-        sct.tmp_copy_nifti(path_tmp + 'centerline.nii',self.input_image.path,self.input_image.file_name+'_centerline'+self.input_image.ext)
+        sct.tmp_copy_nifti(path_tmp + 'centerline.nii.gz',self.input_image.path,self.input_image.file_name+'_centerline'+self.input_image.ext)
 
 
 class SymmetryDetector(Algorithm):
-    def __init__(self, input_image, contrast=None, verbose=0):
+    def __init__(self, input_image, contrast=None, verbose=0, direction="lr"):
         super(SymmetryDetector, self).__init__(input_image)
         self._contrast = contrast
         self._verbose = verbose
+        self.direction = direction
 
     @property
     def contrast(self):
@@ -488,9 +569,12 @@ class SymmetryDetector(Algorithm):
             raise Exception('ERROR: verbose value must be an integer and equal to 0 or 1')
 
     def execute(self):
+        """
+        This function finds the line by line body symmetry from an input image
+        :return: normalised cross-correlation values for the image
+        """
         print 'Execution of the symmetry detection algorithm'
 
-        # for each slice, cut the slice in two at the middle point in the left-right direction
 
 
 if __name__ == "__main__":
